9fb19d1191453cf48426573b4a19effe15eaeda5
darshanmukunda98 <22051109+darshanmukunda98@users.noreply.github.com>
2022-09-19 00:20:32 -0400

    Add files via upload

---
+++ 05_breaking_the_records.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+function breakingRecords(scores) {
+    // Write your code here
+    
+    let highHighest=scores[0];
+    let lowLowest=scores[0];
+    let countL =0;
+    let countH=0;
+    for(let i of scores){
+        
+        if(i<lowLowest){ lowLowest=i; countL++;}
+        if(i>highHighest){ highHighest=i; countH++;}
+        }
+    
+    return [countH, countL];
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const scores = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
+
